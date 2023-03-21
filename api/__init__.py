9fb21b076725f9d2980d500c1f40f2359670acd6
danielogen <danielogen@gmail.com>
2023-03-21 19:41:22 +0300

    ft: config db and flask migrate

--- api/__init__.py
+++ api/__init__.py
@@ -3,11 +3,18 @@ from flask_restx import Api
 from .orders.views import order_namespace
 from .auth.views import auth_namespace
 from .config.config import config_dict
+from .utils import db
+from .models.orders import Order
+from .models.users import User
+from flask_migrate import Migrate
 
 def create_app(config=config_dict['dev']):
     app=Flask(__name__)
 
     app.config.from_object(config)
+
+    db.init_app(app)
+    migrate=Migrate(app, db)
     api = Api(app)
 
     api.add_namespace(order_namespace)
