ac8671c46a90b10fcedc27a359978daef27192d9
danielogen <danielogen@gmail.com>
2023-03-21 19:43:16 +0300

    ft: setup users model

---
+++ api/models/users.py
@@ -0,0 +1,17 @@
+from ..utils import db
+from datetime import datetime
+
+class User(db.Model):
+    __table__='users'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(45), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.Text(), nullable=False)
+    is_staff = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime(), default=datetime.utcnow)
+    orders = db.relationship('Order', backref='customer', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
