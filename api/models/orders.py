4d8e9bffc30d9f49cc2a19be9f58f78b3e7229b6
danielogen <danielogen@gmail.com>
2023-03-21 19:43:31 +0300

    ft: setup orders model

---
+++ api/models/orders.py
@@ -0,0 +1,29 @@
+from ..utils import db
+from enum import Enum
+from datetime import datetime
+
+class Sizes:
+    SMALL = 'small'
+    MEDIUM = 'medium'
+    LARGE = 'large'
+    EXTRA_LARGE = 'extra_large'
+
+class OrderStatus:
+    PENDING = 'pending'
+    IN_TRANSIT = 'in_transit'
+    DELIVERED = 'delivered'
+
+class Order(db.Model):
+    __table__='orders'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    size = db.Column(db.Enum(Sizes), default=Sizes.SMALL)
+    order_status = db.Column(db.Enum(OrderStatus), default=OrderStatus.DELIVERED)
+    flavour = db.Column(db.String(), nullable=False)
+    date_created = db.Column(db.DateTime(), default=datetime.utcnow)
+    user = db.Column(db.Integer(), db.ForeignKey(users.id))
+
+
+    def __repr__(self):
+        return f'<Order {self.id}>'
+
