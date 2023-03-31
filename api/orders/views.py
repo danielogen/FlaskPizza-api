df8e59b886386ccab500342d6bf13c4fdd77da70
danielogen <danielogen@gmail.com>
2023-03-31 14:38:34 +0300

    feat: get all orders

--- api/orders/views.py
+++ api/orders/views.py
@@ -1,16 +1,33 @@
-from flask_restx import Resource,Namespace
+from flask_restx import Resource,Namespace,fields
+from flask_jwt_extended import jwt_required
+from ..models.orders import Order
+from http import HTTPStatus
 
 order_namespace=Namespace('order', description='Namespace for orders')
 
+order_model = order_namespace.model(
+    'Order',{
+        'id':fields.Integer(description="An ID"),
+        'size':fields.String(description="Size of order",required=True,
+            enum=['SMALL','MEDIUM','LARGE','EXTRA_LARGE']
+        ),
+        'order_status':fields.String(description="The status of the Order",
+            required=True, enum=['PENDING','IN_TRANSIT','DELIVERED']
+        )
+    }
+)
 @order_namespace.route('/orders')
 class OrderGetCreate(Resource):
-
+    
+    @order_namespace.marshal_with(order_model)
     def get(self):
         """
             Get all orders
         """
-        pass
-
+        orders=Order.query.all()
+        return orders, HTTPStatus.OK
+    
+    @jwt_required
     def post(self):
         """
             Place an order
