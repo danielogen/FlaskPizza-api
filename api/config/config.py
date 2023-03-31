6c1c1e3a9c6118da4b5340d0fd546c7d8f9349de
danielogen <danielogen@gmail.com>
2023-03-31 05:55:59 +0300

    feat: config jwt

--- api/config/config.py
+++ api/config/config.py
@@ -1,11 +1,15 @@
 import os
 from decouple import config
+from datetime import timedelta
 
 BASE_DIR=os.path.dirname(os.path.realpath(__file__))
 
 class Config:
     SECRET_KEY=config('SECRET_KEY', 'secret')
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+    JWT_ACCESS_TOKEN_EXPIRES=timedelta(minutes=30)
+    JWT_REFRESH_TOKEN_EXPIRES=timedelta(minutes=30)
+    JWT_SECRET_KEY=config('JWT_SECRET_KEY', 'secret')
 
 class DevConfig(Config):
     DEBUG=config('DEBUG', cast=bool)
