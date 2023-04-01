98b608735d7d4f224fff45c300042fb24e451e27
danielogen <danielogen@gmail.com>
2023-04-01 16:26:52 +0300

    feat: setup order testing

---
+++ api/tests/test_orders.py
@@ -0,0 +1,18 @@
+import unittest
+from .. import create_app
+from ..config.config import config_dict
+from ..models.orders import Order
+from ..utils import db
+
+class OrderTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(config=config_dict['test'])
+        self.appctx = self.app.app_context()
+        
+        self.appctx.push()
+
+        self.client = self.app.test_client()
+
+        db.create_all()
+    
+    def tearDown(self):
