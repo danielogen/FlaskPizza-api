7b0742c5bc1e8f8858b11b9234030d727dbb20ec
danielogen <danielogen@gmail.com>
2023-04-01 16:29:29 +0300

    feat: completed test get all orders

--- api/tests/test_orders.py
+++ api/tests/test_orders.py
@@ -3,6 +3,7 @@ from .. import create_app
 from ..config.config import config_dict
 from ..models.orders import Order
 from ..utils import db
+from flask_jwt_extended import create_access_token
 
 class OrderTestCase(unittest.TestCase):
     def setUp(self):
@@ -23,3 +24,18 @@ class OrderTestCase(unittest.TestCase):
         self.appctx.pop()
 
         self.client=None
+
+    def test_get_all_orders(self):
+
+        token=create_access_token(identity='testuser')
+
+        headers={
+            "Authorization":f"Bearer {token}"
+        }
+
+        response=self.client.get('/orders/orders/',headers=headers)
+
+        assert response.status_code == 200
+
+        assert response.json == []
+    
