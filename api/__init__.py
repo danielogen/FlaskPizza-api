dcfdd2a1a819d764fc85fb97f3620a905bb3cac9
danielogen <danielogen@gmail.com>
2023-04-01 16:44:21 +0300

    feat: add swagger doc

--- api/__init__.py
+++ api/__init__.py
@@ -22,15 +22,21 @@ def create_app(config=config_dict['dev']):
             'description':"Add a JWT with ** Bearer &lt;JWT&gt; to authorize"
         }
     }
-
-    db.init_app(app)
-    migrate=Migrate(app, db)
-    jwt=JWTManager(app)
-    
-    api = Api(app)
+    api=Api(app,
+        title="Pizza Delivery API",
+        description="A REST API for a Pizza Delievry service",
+        authorizations=authorizations,
+        security="Bearer Auth"
+    )
 
     api.add_namespace(order_namespace)
     api.add_namespace(auth_namespace, path='/auth')
+
+    db.init_app(app)
+
+    migrate=Migrate(app, db)
+
+    jwt=JWTManager(app)
 
     @app.shell_context_processor
     def make_shell_context():
